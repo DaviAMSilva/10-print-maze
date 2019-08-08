88fd14fcfb42ddd3ace06c381d9b92a8ccc46ca1
Davi Silva <daviamsilva@hotmail.com>
2019-08-08 17:53:43 -0300

    Bug fix

--- Heuristic.js
+++ Heuristic.js
@@ -2,10 +2,7 @@
 
 function heuristic(a, b) {
     // Cartesian distance
-    // var d = dist(a.x, a.y, b.x, b.y);
-
-    // Manhattan distance
-    // var d = abs(a.i - b.i) + abs(a.j - b.j);
+    var d = dist(a.x, a.y, b.x, b.y);
 
     return d;
 }
